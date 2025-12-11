764cbe9fec4da6c076c0b9f09f79adb61d550bda
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-12-12 00:34:42 +0100

    feat: ajout de l'entité book publisher

---
+++ projet-s1-symfony/src/Entity/BookPublisher.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookPublisherRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookPublisherRepository::class)]
+class BookPublisher
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $publisherName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contactEmail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $website = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPublisherName(): ?string
+    {
+        return $this->publisherName;
+    }
+
+    public function setPublisherName(string $publisherName): static
+    {
+        $this->publisherName = $publisherName;
+
+        return $this;
+    }
+
+    public function getContactEmail(): ?string
+    {
+        return $this->contactEmail;
+    }
+
+    public function setContactEmail(string $contactEmail): static
+    {
+        $this->contactEmail = $contactEmail;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(string $website): static
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+}
