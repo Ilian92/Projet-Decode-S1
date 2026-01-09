eed68c52e081ef61440878aee86bc59fc3617664
Ilian Igoudjil <ilianigoudjil@gmail.com>
2026-01-09 17:04:31 +0100

    feat: controller d'insertion dans la bdd des informations en rapport à un livre

---
+++ projet-s1-symfony/src/Controller/DatabaseInsertionController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Entity\Genre;
+use App\Entity\Work;
+use App\Entity\Book;
+use App\Entity\BookPublisher;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
+final class DatabaseInsertionController extends AbstractController
+{
+    // rajouter commentaire sur ce qui est attendu en input et fait ici
+    #[Route('/database/insertion', name: 'app_database_insertion', methods: ['POST'])]
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer
+    ): Response {
+        try {
+            $data = json_decode($request->getContent(), true);
+
+            if (!$data) {
+                return new JsonResponse([
+                    'success' => false,
+                    'error' => 'Invalid JSON format'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            // Auhor
+            $authors = [];
+            if (isset($data['authors']) && is_array($data['authors'])) {
+                foreach ($data['authors'] as $authorData) {
+                    $author = $serializer->deserialize(
+                        json_encode($authorData),
+                        Author::class,
+                        'json'
+                    );
+
+                    $entityManager->persist($author);
+                    $authors[] = $author;
+                }
+            }
+
+            // Genre
+            $genres = [];
+            if (isset($data['genres']) && is_array($data['genres'])) {
+                foreach ($data['genres'] as $genreData) {
+                    $genre = $serializer->deserialize(
+                        json_encode($genreData),
+                        Genre::class,
+                        'json'
+                    );
+
+                    $entityManager->persist($genre);
+                    $genres[] = $genre;
+                }
+            }
+
+            // BookPublisher
+            $bookPublisher = null;
+            if (isset($data['bookPublisher'])) {
+                $bookPublisher = $serializer->deserialize(
+                    json_encode($data['bookPublisher']),
+                    BookPublisher::class,
+                    'json'
+                );
+
+                $entityManager->persist($bookPublisher);
+            }
+
+            // Work and relations
+            $work = null;
+            if (isset($data['work'])) {
+                $authorIds = $data['work']['authorIds'] ?? [];
+                $genreIds = $data['work']['genreIds'] ?? [];
+
+                unset($data['work']['authorIds'], $data['work']['genreIds']);
+
+                $work = $serializer->deserialize(
+                    json_encode($data['work']),
+                    Work::class,
+                    'json'
+                );
+
+                foreach ($authorIds as $authorId) {
+                    if (isset($authors[$authorId])) {
+                        $work->addAuthor($authors[$authorId]);
+                    }
+                }
+
+                foreach ($genreIds as $genreId) {
+                    if (isset($genres[$genreId])) {
+                        $work->addGenre($genres[$genreId]);
+                    }
+                }
+
+                $entityManager->persist($work);
+            }
+
+            // Book and relations
+            $book = null;
+            if (isset($data['book']) && $work) {
+                $book = $serializer->deserialize(
+                    json_encode($data['book']),
+                    Book::class,
+                    'json'
+                );
+
+                $book->setWork($work);
+
+                if ($bookPublisher) {
+                    $book->setBookPublisher($bookPublisher);
+                }
+
+                $entityManager->persist($book);
+            }
+
+            // Save all to database
+            $entityManager->flush();
+
+            return new JsonResponse([
+                'success' => true,
+                'message' => 'Data successfully inserted',
+                'ids' => [
+                    'authors' => array_map(fn($a) => $a->getId(), $authors),
+                    'genres' => array_map(fn($g) => $g->getId(), $genres),
+                    'work' => $work?->getId(),
+                    'bookPublisher' => $bookPublisher?->getId(),
+                    'book' => $book?->getId(),
+                ]
+            ], Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
