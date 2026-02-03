ef63209bb626efef230eaf274ce6a1aaf921e924
Ilian Igoudjil <ilianigoudjil@gmail.com>
2026-02-03 12:12:26 +0100

    fix: routes des crud admin

--- projet-s1-symfony/src/Controller/AdminMonthlyBoxController.php
+++ projet-s1-symfony/src/Controller/AdminMonthlyBoxController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/monthly/box')]
+#[Route('/admin/monthly-box')]
 final class AdminMonthlyBoxController extends AbstractController
 {
     #[Route(name: 'app_admin_monthly_box_index', methods: ['GET'])]
--- projet-s1-symfony/src/Controller/AdminOrderLineController.php
+++ projet-s1-symfony/src/Controller/AdminOrderLineController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/order/line')]
+#[Route('/admin/order-line')]
 final class AdminOrderLineController extends AbstractController
 {
     #[Route(name: 'app_admin_order_line_index', methods: ['GET'])]
