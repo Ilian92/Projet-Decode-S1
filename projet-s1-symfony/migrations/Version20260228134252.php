ee9daa56f82f63d18cfcec4a1c6d2cab48a9c177
Ilian Igoudjil <ilianigoudjil@gmail.com>
2026-03-06 14:56:04 +0100

    fix: migrations cleaned

--- projet-s1-symfony/migrations/Version20260228134252.php
+++ projet-s1-symfony/migrations/Version20260228134252.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260228134252 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,14 +16,12 @@ final class Version20260228134252 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE book ADD edition_olid VARCHAR(32) DEFAULT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_CBE5A3314D1B94BC ON book (edition_olid)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP INDEX UNIQ_CBE5A3314D1B94BC');
         $this->addSql('ALTER TABLE book DROP edition_olid');
